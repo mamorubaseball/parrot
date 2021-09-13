891ae05a14e85366efad7a505b8481134c843072
mamoru <62536753+mamorubaseball@users.noreply.github.com>
2021-09-13 11:54:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-from __future__ import print_function  # python2/3 compatibility for the print function
+# -*- coding: UTF-8 -*-
+
 import olympe
-from olympe.messages.ardrone3.PilotingState import PositionChanged
-from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+import time
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing
-from olympe.messages.ardrone3.GPSSettingsState import HomeChanged
 
-# Connection
-drone = olympe.Drone("192.168.42.1")
-drone.connection()
+DRONE_IP = "192.168.42.1"
 
-# Wait for GPS fix
-drone(GPSFixStateChanged(_policy = 'wait'))
 
-print("GPS position before take-off :", drone.get_state(HomeChanged))
+def main():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    assert drone(TakeOff()).wait().success()
+    time.sleep(10)
+    assert drone(Landing()).wait().success()
+    drone.disconnect()
 
-# Take-off
-drone(TakeOff()).wait()
 
-print("GPS position after take-off : ", drone.get_state(PositionChanged))
-drone(Landing()).wait().success()
-
-drone.disconnection()
+if __name__ == "__main__":
+    main()
