45abd5a7c1b664a11134e91448bf3e0e62713fd2
mamoru <62536753+mamorubaseball@users.noreply.github.com>
2021-09-13 09:17:32 +0900

    Create landing.py

---
+++ landing.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+import olympe
+import time
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+DRONE_IP = "192.168.42.1"
+def main():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    assert drone(Landing()).wait().success()
+    time.sleep(10)
+if __name__ == "__main__":
+    main()
