e3706e15ac9c9b91e4ec50f3bab9e0898414af7e
mamoru <62536753+mamorubaseball@users.noreply.github.com>
2021-09-16 14:37:16 +0900

    Add files via upload

---
+++ moveby_from_GPS.py
@@ -0,0 +1,81 @@
+
+from __future__ import print_function  # python2/3 compatibility for the print function
+import olympe
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveTo,moveBy
+from olympe.messages.ardrone3.PilotingState import PositionChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+from olympe.messages.ardrone3.GPSSettingsState import HomeChanged
+from olympe.messages.ardrone3.PilotingState import FlyingStateChanged,moveToChanged
+from olympe.enums.ardrone3.PilotingState import MoveToChanged_Status as status
+from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
+import olympe.enums.move as mode
+import math
+import os, csv, time, tempfile
+
+start = [35.7099482, 139.5230989, 1.0]
+p0 = [35.7099068, 139.5231090, 1.0]
+goal = [35.709901, 139.523350, 1.0]
+
+def get_now_gps(drone):
+    # Wait for GPS fix
+    drone(GPSFixStateChanged(_policy='wait'))
+    return drone.get_state(HomeChanged)
+
+def calcurate(drone):
+    gps=get_now_gps(drone)
+    print('='*10)
+    print(gps)
+    print(gps[0])
+    print(gps['latitude'])
+    lat1,log1,alt1=gps[0],gps[1],gps[2]
+    lat2,log2,alt2=p0[0],p0[1],p0[2]
+    disctance=get_distance(lat1,log1,lat2,log2,precision=8)
+    direction=get_direction(lat1,log1,lat2,log2)
+    x=disctance*math.cos(math.radians(direction))
+    y=disctance*math.sin(math.radians(direction))
+    z=0
+    return x,y,z,direction
+
+def get_distance(lat1,log1,lat2,log2,precision):
+    distance=0
+    if abs(lat1-lat2)<0.00001 and abs(log1-log2)<0.00001:
+        distance=0
+    else:
+        lat1=lat1*math.pi/180
+        lat2=lat2*math.pi/180
+        log1=log1*math.pi/180
+        log2=log2*math.pi/180
+        A = 6378140
+        B = 6356755
+        F = (A - B) / A
+        P1=math.atan((B/A)*math.tan(lat1))
+        P2=math.atan((B/A)*math.tan(lat2))
+        X=math.acos(math.sin(P1)*math.sin(P2)+math.cos(P1)*math.cos(P2)*math.cos(log1-log2))
+        L=(F/8)*((math.sin(X)-X)*math.pow((math.sin(P1)+math.sin(P2)),2)/math.pow(math.cos(X/2),2)-(math.sin(X)-X)*math.pow(math.sin(P1)-math.sin(P2),2)/math.pow(math.sin(X),2))
+        distance = A * (X + L)
+        decimal_no = math.pow(10, precision)
+        distance = round(decimal_no * distance / 1) / decimal_no
+        return distance
+def get_direction(lat1,log1,lat2,log2):
+    Y = math.cos(log2 * math.pi / 180) * math.sin(lat2 * math.pi / 180 - lat1 * math.pi / 180);
+    X = math.cos(log1 * math.pi / 180) * math.sin(log2 * math.pi / 180) - math.sin(log1 * math.pi / 180) * math.cos(
+        log2 * math.pi / 180) * math.cos(lat2 * math.pi / 180 - lat1 * math.pi / 180)
+    dirE0 = 180 * math.atan2(Y, X) / math.pi;#東向けがゼロ
+    if dirE0<0:
+        dirE0+=360
+    dirN0=(dirE0+90)%360
+    return dirN0 #北をゼロとして、角度う
+
+def main():
+    drone = olympe.Drone("192.168.42.1")
+    drone.connection()
+    x,y,z,direction=calcurate(drone)
+    drone(moveBy(y, x, z, direction)
+        >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
+    print('=====================GOAL=====================================')
+    drone(Landing()).wait()
+    print("GPS position after take-off : ", drone.get_state(PositionChanged))
+    drone.disconnection()
+
+if __name__ == '__main__':
+    main()
