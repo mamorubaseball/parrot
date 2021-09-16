a8090602e3291662bf96a3aaed7c164954554e05
mamoru <62536753+mamorubaseball@users.noreply.github.com>
2021-09-16 20:21:54 +0900

    Update moveby_from_GPS.py

--- moveby_from_GPS.py
+++ moveby_from_GPS.py
@@ -1,7 +1,7 @@
 
 from __future__ import print_function  # python2/3 compatibility for the print function
 import olympe
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveTo,moveBy
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveTo,moveBy,Circle, PCMD
 from olympe.messages.ardrone3.PilotingState import PositionChanged
 from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
 from olympe.messages.ardrone3.GPSSettingsState import HomeChanged
@@ -80,10 +80,13 @@ def main():
 
     x,y,z,direction=calcurate(drone,p0)
     drone(moveBy(y, x, z, direction)
+        >> PCMD(1, 0, 0, 0, 0, 0)
         >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
+    
     print('=====================p0=====================================')
     x,y,z,direction=calcurate(drone,goal)
     drone(moveBy(y, x, z, direction)
+        >> PCMD(1, 0, 0, 0, 0, 0)
         >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
     print('=====================GOAL=====================================')
     drone(Landing()).wait()
