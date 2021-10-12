9ca1c9203c7c9bbd99f344f1536e737a3c6bf8a3
mamorubaseball <mamorubasebaii9045@gmail.com>
2021-10-13 00:42:42 +0900

    test

--- test.py
+++ test.py
@@ -21,24 +21,18 @@ import olympe.enums.move as mode
 import math
 from math import sin, cos, tan, atan2, acos, pi
 import os, csv, time, tempfile
-# from phote import *
+from phote import *
 import csv
 import pandas as pd
-
+from move_phote_from_GPS import *
 DRONE_IP = "192.168.42.1"
 SKYCTRL_IP = "192.168.53.1"
 
-start = [35.7099482, 139.5230989, 1.0]
-p0 = [35.7099068, 139.5231090, 1.0]
-goal = [35.709901, 139.523350, 1.0]
 
-def main():
-    drone = olympe.Drone(DRONE_IP)
-    drone.connect()
-    assert drone(TakeOff()).wait().success()
-    time.sleep(10)
-    assert drone(Landing()).wait().success()
-    drone.disconnect()
+#3DのGPSポイント
+start = [35.70988794,139.5231035,1.5657513]
+p0 = [35.70983231,139.5230996,1.5657513]
+goal = [35.7096731,139.5230775,1.5657513]
 
 def move_to(drone):
     assert drone(TakeOff()).wait().success()
@@ -50,8 +44,6 @@ def move_to(drone):
     drone(extended_move_to(latitude=goal[0], longitude=goal[1], altitude=goal[2],max_horizontal_speed=0.1))
     assert drone(Landing()).wait().success()
     time.sleep(3)
-
-
 
 def move_by(drone):
     sita=int(input())
@@ -114,7 +106,58 @@ def test_move_sita():
 
     drone(Landing()).wait().success()
 
+#3D化するためのポイント合計4*4
+#G:35.709825276365, 139.52321058567497,
 
+def test1():
+    start = time.time()
+    drone = olympe.Drone("192.168.42.1")
+    drone.connection()
+    drone_direction=0
+    set_gimbal(drone)
+    time.sleep(2)
+    df=pd.read_csv('CSV/3D.csv')
+    assert drone(TakeOff()
+                 >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
+    for i,d in df.iterrows():
+        #２分以上の飛行をNGとする
+        if time.time()-start>120:
+            print('=========２分以上の飛行========')
+            break
+        if i>5:
+            break
+        gps=[d[0],d[1],d[2]]
+        direct=move_take_phote(drone, gps,drone_direction)
+        drone_direction=direct
+        print('======現在地点{}==========='.format(gps))
+        print('======ドローン方向{}==========='.format(drone_direction))
+        time.sleep(2)
+    drone(Landing()).wait()
+    drone_gps = drone.get_state(PositionChanged)
+
+def test2():
+    start = time.time()
+    drone = olympe.Drone("192.168.42.1")
+    drone.connection()
+    drone_direction = 0
+    set_gimbal(drone)
+    time.sleep(5)
+    df = pd.read_csv('CSV/3D.csv')
+    assert drone(TakeOff()
+                 >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
+    for i, d in df.iterrows():
+        # ２分以上の飛行をNGとする
+        if time.time() - start > 120:
+            print('=========２分以上の飛行========')
+            break
+        drone(moveTo(d[0], d[1], d[2], MoveTo_Orientation_mode.TO_TARGET, 0.0)
+              >> moveToChanged(status='hovering', _timeout=10)).wait()
+        time.sleep(3)
+        setup_photo_burst_mode(drone)
+        take_photo_burst(drone)
+    drone(Landing()).wait()
+    drone_gps = drone.get_state(PositionChanged)
+    print(get_distance(goal[0], goal[1], drone_gps['latitude'], drone_gps['longitude'], 8))
 # def conect_skycontroller():
 #     drone = olympe.Drone(SKYCTRL_IP)
 #     drone.connect()
@@ -122,4 +165,11 @@ def test_move_sita():
 #     drone.disconnect()
 
 if __name__ == "__main__":
-    test_move_sita()
+    test1()
+    test2()
+
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    move_by(drone)
+    move_to(drone)
+
