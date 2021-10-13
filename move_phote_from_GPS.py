c07f7e82a14081f9e8f5d1ff5c439b648695bba9
mamorubaseball <mamorubasebaii9045@gmail.com>
2021-10-13 12:19:52 +0900

    m

--- move_phote_from_GPS.py
+++ move_phote_from_GPS.py
@@ -1,6 +1,7 @@
 from __future__ import print_function  # python2/3 compatibility for the print function
 import olympe
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveTo, moveBy, Circle, PCMD
+from olympe.messages.move import extended_move_by,extended_move_to
 from olympe.messages.ardrone3.PilotingState import PositionChanged
 from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
 from olympe.messages.ardrone3.GPSSettingsState import HomeChanged
@@ -9,9 +10,11 @@ from olympe.enums.ardrone3.PilotingState import MoveToChanged_Status as status
 from olympe.enums.ardrone3.Piloting import MoveTo_Orientation_mode
 import olympe.enums.move as mode
 import math
+from math import *
 from math import sin, cos, tan, atan2, acos, pi
 import os, csv, time, tempfile
 from phote import *
+
 import csv
 import pandas as pd
 
@@ -76,35 +79,130 @@ def get_distance(lat1, log1, lat2, log2, precision):
         return distance
 
 def get_direction(lat1, log1, lat2, log2):
-    Y = math.cos(log2 * math.pi / 180) * math.sin(lat2 * math.pi / 180 - lat1 * math.pi / 180);
+    Y = math.cos(log2 * math.pi / 180) * math.sin(lat2 * math.pi / 180 - lat1 * math.pi / 180)
     X = math.cos(log1 * math.pi / 180) * math.sin(log2 * math.pi / 180) - math.sin(log1 * math.pi / 180) * math.cos(
         log2 * math.pi / 180) * math.cos(lat2 * math.pi / 180 - lat1 * math.pi / 180)
-    dirE0 = 180 * math.atan2(Y, X) / math.pi;  # 東向けがゼロ
+    dirE0 = 180 * math.atan2(Y, X) / math.pi # 東向けがゼロ
     if dirE0 < 0:
         dirE0 += 360
     dirN0 = (dirE0 + 90) % 360
     dirN0 = dirN0 / 360 * math.pi
     return dirN0  # 北をゼロとして、角
 
-#kakudo
-# def azimuth(x1, y1, x2, y2):
-#     # Radian角に修正
-#     _x1, _y1, _x2, _y2 = x1*pi/180, y1*pi/180, x2*pi/180, y2*pi/180
-#     Δx = _x2 - _x1
-#     _y = sin(Δx)
-#     _x = cos(_y1) * tan(_y2) - sin(_y1) * cos(Δx)
 
-#     psi = atan2(_y, _x) * 180 / pi
-#     if psi < 0:
-#         return 360 + atan2(_y, _x) * 180 / pi
-#     else:
-#         return atan2(_y, _x) * 180 / pi
-# #距離
-# def distance(x1, y1, x2, y2, 6378.137e3):
-#     _x1, _y1, _x2, _y2 = x1*pi/180, y1*pi/180, x2*pi/180, y2*pi/180
-#     Δx = _x2 - _x1
-#     val = sin(_y1) * sin(_y2) + cos(_y1) * cos(_y2) * cos(Δx)
-#     return r * acos(val)*1000
+
+# 楕円体
+ELLIPSOID_GRS80 = 1 # GRS80
+ELLIPSOID_WGS84 = 2 # WGS84
+
+# 楕円体ごとの長軸半径と扁平率
+GEODETIC_DATUM = {
+    ELLIPSOID_GRS80: [
+        6378137.0,         # [GRS80]長軸半径
+        1 / 298.257222101, # [GRS80]扁平率
+    ],
+    ELLIPSOID_WGS84: [
+        6378137.0,         # [WGS84]長軸半径
+        1 / 298.257223563, # [WGS84]扁平率
+    ],
+}
+
+# 反復計算の上限回数
+ITERATION_LIMIT = 1000
+
+'''
+Vincenty法(逆解法)
+2地点の座標(緯度経度)から、距離と方位角を計算する
+:param lat1: 始点の緯度
+:param lon1: 始点の経度
+:param lat2: 終点の緯度
+:param lon2: 終点の経度
+:param ellipsoid: 楕円体
+:return: 距離と方位角
+'''
+def vincenty_inverse(lat1, lon1, lat2, lon2, ellipsoid=None):
+
+    # 差異が無ければ0.0を返す
+    if isclose(lat1, lat2) and isclose(lon1, lon2):
+        return {
+            'distance': 0.0,
+            'azimuth1': 0.0,
+            'azimuth2': 0.0,
+        }
+
+    # 計算時に必要な長軸半径(a)と扁平率(ƒ)を定数から取得し、短軸半径(b)を算出する
+    # 楕円体が未指定の場合はGRS80の値を用いる
+    a, ƒ = GEODETIC_DATUM.get(ellipsoid, GEODETIC_DATUM.get(ELLIPSOID_GRS80))
+    b = (1 - ƒ) * a
+
+    φ1 = radians(lat1)
+    φ2 = radians(lat2)
+    λ1 = radians(lon1)
+    λ2 = radians(lon2)
+
+    # 更成緯度(補助球上の緯度)
+    U1 = atan((1 - ƒ) * tan(φ1))
+    U2 = atan((1 - ƒ) * tan(φ2))
+
+    sinU1 = sin(U1)
+    sinU2 = sin(U2)
+    cosU1 = cos(U1)
+    cosU2 = cos(U2)
+
+    # 2点間の経度差
+    L = λ2 - λ1
+
+    # λをLで初期化
+    λ = L
+
+    # 以下の計算をλが収束するまで反復する
+    # 地点によっては収束しないことがあり得るため、反復回数に上限を設ける
+    for i in range(ITERATION_LIMIT):
+        sinλ = sin(λ)
+        cosλ = cos(λ)
+        sinσ = sqrt((cosU2 * sinλ) ** 2 + (cosU1 * sinU2 - sinU1 * cosU2 * cosλ) ** 2)
+        cosσ = sinU1 * sinU2 + cosU1 * cosU2 * cosλ
+        σ = atan2(sinσ, cosσ)
+        sinα = cosU1 * cosU2 * sinλ / sinσ
+        cos2α = 1 - sinα ** 2
+        cos2σm = cosσ - 2 * sinU1 * sinU2 / cos2α
+        C = ƒ / 16 * cos2α * (4 + ƒ * (4 - 3 * cos2α))
+        λʹ = λ
+        λ = L + (1 - C) * ƒ * sinα * (σ + C * sinσ * (cos2σm + C * cosσ * (-1 + 2 * cos2σm ** 2)))
+
+        # 偏差が.000000000001以下ならbreak
+        if abs(λ - λʹ) <= 1e-12:
+            break
+    else:
+        # 計算が収束しなかった場合はNoneを返す
+        return None
+
+    # λが所望の精度まで収束したら以下の計算を行う
+    u2 = cos2α * (a ** 2 - b ** 2) / (b ** 2)
+    A = 1 + u2 / 16384 * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+    B = u2 / 1024 * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
+    Δσ = B * sinσ * (cos2σm + B / 4 * (cosσ * (-1 + 2 * cos2σm ** 2) - B / 6 * cos2σm * (-3 + 4 * sinσ ** 2) * (-3 + 4 * cos2σm ** 2)))
+
+    # 2点間の楕円体上の距離
+    s = b * A * (σ - Δσ)
+
+    # 各点における方位角
+    α1 = atan2(cosU2 * sinλ, cosU1 * sinU2 - sinU1 * cosU2 * cosλ)
+    α2 = atan2(cosU1 * sinλ, -sinU1 * cosU2 + cosU1 * sinU2 * cosλ) + pi
+
+    if α1 < 0:
+        α1 = α1 + pi * 2
+
+    return {
+        'distance': s,           # 距離
+        'azimuth1': degrees(α1), # 方位角(始点→終点)
+        'azimuth2': degrees(α2), # 方位角(終点→始点)
+    }
+
+
+
+
+
 
 #回転させて進んだら写真撮る関数[pは
 def move_take_phote(drone,p,drone_direction):
@@ -122,6 +220,28 @@ def move_take_phote(drone,p,drone_direction):
     setup_photo_burst_mode(drone)
     take_photo_burst(drone)
     return direction
+def move_take_phote_2(drone,p,drone_direction):
+    gps = get_now_gps(drone)
+    lat1,log1=gps[0],gps[1]
+    lat2,log2=p[0],p[1]
+    result = vincenty_inverse(lat1, log1, lat2, log2, 1)
+    distance = round(result['distance'], 3)
+    direction = result['azimuth1']
+    sita=direction-drone_direction
+    sita = (sita / 180) * pi
+    print('============sita={}==========='.format(str(sita)))
+    if distance>6:
+        distance=6
+    drone(moveBy(0, 0, 0, sita)
+          >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
+
+    drone(extended_move_by(distance,0,0,0, max_horizontal_speed=0.1)
+          >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
+
+    setup_photo_burst_mode(drone)
+    take_photo_burst(drone)
+    return direction
+
 
 def move_take_phote_sita(drone,distance,sita):
     if distance>4:
@@ -171,29 +291,14 @@ def main():
     assert drone(TakeOff()
                  >> FlyingStateChanged(state="hovering", _timeout=5)).wait().success()
 
-    # for i in range(len(df)-1):
-    #     print('===========droneGPS{}====='.format(get_now_gps(drone)))
-    #     if time.time() - start > 120:
-    #         print('=========２分以上の飛行========')
-    #         break
-    #     drone_p=df.loc[i,:]
-    #     gps=df.loc[i+1,:]
-    #     lat1,log1=drone_p[0],drone_p[1]
-    #     lat2,log2=gps[0],gps[1]
-    #     distance = get_distance(lat1, log1, lat2, log2, 8)
-    #     direction = get_direction(lat1, log1, lat2, log2)
-    #     sita=drone_direction-direction
-    #     move_take_phote_sita(drone,distance,sita)
-    #     drone_direction=direction
-    #     print('distance='+str(distance))
-    #     print('direction'+str(direction))
+    drone_direction = 0
     for i,d in df.iterrows():
         #２分以上の飛行をNGとする
         if time.time()-start>120:
             print('=========２分以上の飛行========')
             break
         gps=[d[0],d[1],d[2]]
-        direct=move_take_phote(drone, gps,drone_direction)
+        direct=move_take_phote_2(drone, gps,drone_direction)
         drone_direction=direct
         print('======現在地点{}==========='.format(gps))
         print('======ドローン方向{}==========='.format(drone_direction))
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import pandas as pd
 # from move_phote_from_GPS import get_distance,get_direction
 import time
 import math
+from math import *
 
 def get_distance(lat1, log1, lat2, log2, precision):
     distance = 0
@@ -56,24 +57,141 @@ def get_direction(lat1, log1, lat2, log2):
     dirN0 = dirN0 / 360 * math.pi
     return dirN0  # 北をゼロとして、角
 
+
+# 楕円体
+ELLIPSOID_GRS80 = 1 # GRS80
+ELLIPSOID_WGS84 = 2 # WGS84
+
+# 楕円体ごとの長軸半径と扁平率
+GEODETIC_DATUM = {
+    ELLIPSOID_GRS80: [
+        6378137.0,         # [GRS80]長軸半径
+        1 / 298.257222101, # [GRS80]扁平率
+    ],
+    ELLIPSOID_WGS84: [
+        6378137.0,         # [WGS84]長軸半径
+        1 / 298.257223563, # [WGS84]扁平率
+    ],
+}
+
+# 反復計算の上限回数
+ITERATION_LIMIT = 1000
+
+'''
+Vincenty法(逆解法)
+2地点の座標(緯度経度)から、距離と方位角を計算する
+:param lat1: 始点の緯度
+:param lon1: 始点の経度
+:param lat2: 終点の緯度
+:param lon2: 終点の経度
+:param ellipsoid: 楕円体
+:return: 距離と方位角
+'''
+def vincenty_inverse(lat1, lon1, lat2, lon2, ellipsoid=None):
+
+    # 差異が無ければ0.0を返す
+    if isclose(lat1, lat2) and isclose(lon1, lon2):
+        return {
+            'distance': 0.0,
+            'azimuth1': 0.0,
+            'azimuth2': 0.0,
+        }
+
+    # 計算時に必要な長軸半径(a)と扁平率(ƒ)を定数から取得し、短軸半径(b)を算出する
+    # 楕円体が未指定の場合はGRS80の値を用いる
+    a, ƒ = GEODETIC_DATUM.get(ellipsoid, GEODETIC_DATUM.get(ELLIPSOID_GRS80))
+    b = (1 - ƒ) * a
+
+    φ1 = radians(lat1)
+    φ2 = radians(lat2)
+    λ1 = radians(lon1)
+    λ2 = radians(lon2)
+
+    # 更成緯度(補助球上の緯度)
+    U1 = atan((1 - ƒ) * tan(φ1))
+    U2 = atan((1 - ƒ) * tan(φ2))
+
+    sinU1 = sin(U1)
+    sinU2 = sin(U2)
+    cosU1 = cos(U1)
+    cosU2 = cos(U2)
+
+    # 2点間の経度差
+    L = λ2 - λ1
+
+    # λをLで初期化
+    λ = L
+
+    # 以下の計算をλが収束するまで反復する
+    # 地点によっては収束しないことがあり得るため、反復回数に上限を設ける
+    for i in range(ITERATION_LIMIT):
+        sinλ = sin(λ)
+        cosλ = cos(λ)
+        sinσ = sqrt((cosU2 * sinλ) ** 2 + (cosU1 * sinU2 - sinU1 * cosU2 * cosλ) ** 2)
+        cosσ = sinU1 * sinU2 + cosU1 * cosU2 * cosλ
+        σ = atan2(sinσ, cosσ)
+        sinα = cosU1 * cosU2 * sinλ / sinσ
+        cos2α = 1 - sinα ** 2
+        cos2σm = cosσ - 2 * sinU1 * sinU2 / cos2α
+        C = ƒ / 16 * cos2α * (4 + ƒ * (4 - 3 * cos2α))
+        λʹ = λ
+        λ = L + (1 - C) * ƒ * sinα * (σ + C * sinσ * (cos2σm + C * cosσ * (-1 + 2 * cos2σm ** 2)))
+
+        # 偏差が.000000000001以下ならbreak
+        if abs(λ - λʹ) <= 1e-12:
+            break
+    else:
+        # 計算が収束しなかった場合はNoneを返す
+        return None
+
+    # λが所望の精度まで収束したら以下の計算を行う
+    u2 = cos2α * (a ** 2 - b ** 2) / (b ** 2)
+    A = 1 + u2 / 16384 * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+    B = u2 / 1024 * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
+    Δσ = B * sinσ * (cos2σm + B / 4 * (cosσ * (-1 + 2 * cos2σm ** 2) - B / 6 * cos2σm * (-3 + 4 * sinσ ** 2) * (-3 + 4 * cos2σm ** 2)))
+
+    # 2点間の楕円体上の距離
+    s = b * A * (σ - Δσ)
+
+    # 各点における方位角
+    α1 = atan2(cosU2 * sinλ, cosU1 * sinU2 - sinU1 * cosU2 * cosλ)
+    α2 = atan2(cosU1 * sinλ, -sinU1 * cosU2 + cosU1 * sinU2 * cosλ) + pi
+
+    if α1 < 0:
+        α1 = α1 + pi * 2
+
+    return {
+        'distance': s,           # 距離
+        'azimuth1': degrees(α1), # 方位角(始点→終点)
+        'azimuth2': degrees(α2), # 方位角(終点→始点)
+    }
+
 def simulation():
-    df = pd.read_csv('CSV/GPS10_1.csv')
+    df = pd.read_csv('CSV/3D.csv')
     start = time.time()
     # print(df.loc[2,:])
 
     drone_direction = 0
-    for i in range(len(df)-1):
+    for i in range(0,len(df)-1):
         drone=df.loc[i,:]
         gps=df.loc[i+1,:]
         lat1,log1=drone[0],drone[1]
         lat2,log2=gps[0],gps[1]
-        distance = get_distance(lat1, log1, lat2, log2, 8)
-        direction = get_direction(lat1, log1, lat2, log2)
-        sita=drone_direction-direction
+        result = vincenty_inverse(lat1, log1, lat2, log2, 1)
+        distance=round(result['distance'], 3)
+        direction=result['azimuth1']
+        sita=direction-drone_direction
+        print(sita)
+        print(distance)
+        drone_direction=direction
 
-        drone_direction = direction
-        print('distance='+str(distance))
-        print('direction'+str(direction))
+
+        # print('distance={}'.format(round(result['distance'], 3)))
+        # print('方位角度{}'.format(result['azimuth1']))
         print('======={}======'.format(i))
 
-simulation()
+def pandas():
+    df=pd.read_csv('CSV/3D.csv')
+    for i, d in df.iterrows():
+        print(d[0],d[1])
+pandas()
