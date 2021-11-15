1837c2d1ac035f715f6728d37f8ad505909e6cc2
mamoru <62536753+mamorubaseball@users.noreply.github.com>
2021-11-15 21:47:00 +0900

    Update landing.py

--- landing.py
+++ landing.py
@@ -2,12 +2,16 @@
 import olympe
 import time
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+import os
 
-DRONE_IP = "192.168.42.1"
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
+
+
 def main():
     drone = olympe.Drone(DRONE_IP)
     drone.connect()
     assert drone(Landing()).wait().success()
     time.sleep(10)
+   
 if __name__ == "__main__":
     main()
