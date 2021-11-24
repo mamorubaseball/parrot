b2c497e44d7527b2060bfc28223d63c9c4aa80f3
mamoru <62536753+mamorubaseball@users.noreply.github.com>
2021-11-24 15:35:20 +0900

    Update phote.py

--- phote.py
+++ phote.py
@@ -1,30 +1,35 @@
-# -*- coding: UTF-8 -*-
-#!/usr/bin/python3
+
 from olympe.messages.camera import (
     set_camera_mode,
     set_photo_mode,
     take_photo,
     photo_progress,
 )
-from olympe.messages import gimbal
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+from olympe.media import download_media, indexing_state
+from logging import getLogger
 import olympe
 import os
 import re
-import csv
-import requests
-import shutil
 import tempfile
 import xml.etree.ElementTree as ET
-import time
 
+
+olympe.log.update_config({
+    "loggers": {
+        "olympe": {"level": "INFO"},
+        "photo_example": {
+            "level": "INFO",
+            "handlers": ["console"],
+        },
+    }
+})
+
+logger = getLogger("photo_example")
 
 # Drone IP
-ANAFI_IP = "192.168.42.1"
-# Drone web server URL
-ANAFI_URL = "http://{}/".format(ANAFI_IP)
-# Drone media web API URL
-ANAFI_MEDIA_API_URL = ANAFI_URL + "api/v1/media/medias/"
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
+DRONE_MEDIA_PORT = os.environ.get("DRONE_MEDIA_PORT", "80")
+
 XMP_TAGS_OF_INTEREST = (
     "CameraRollDegree",
     "CameraPitchDegree",
@@ -38,86 +43,84 @@ XMP_TAGS_OF_INTEREST = (
 )
 
 
-def takeoff(drone):
-    drone(TakeOff()).wait().success()
-
-
-def land(drone):
-    drone(Landing()).wait().success()
-
 def take_photo_burst(drone):
     # take a photo burst and get the associated media_id
     photo_saved = drone(photo_progress(result="photo_saved", _policy="wait"))
     drone(take_photo(cam_id=0)).wait()
-    photo_saved.wait()
-    media_id = photo_saved.received_events().last().args["media_id"]
-
+    if not photo_saved.wait(_timeout=30).success():
+        assert False, "take_photo timedout"
+    photo_progress_info = photo_saved.received_events().last().args
+    media_id = photo_progress_info["media_id"]
+    photo_count = photo_progress_info["photo_count"]
     # download the photos associated with this media id
-    media_info_response = requests.get(ANAFI_MEDIA_API_URL + media_id)
-    media_info_response.raise_for_status()
-    download_dir = tempfile.mkdtemp()
-    for resource in media_info_response.json()["resources"]:
-        image_response = requests.get(ANAFI_URL + resource["url"], stream=True)
-        download_path = os.path.join(download_dir, resource["resource_id"])
-        image_response.raise_for_status()
-        with open(download_path, "wb") as image_file:
-            shutil.copyfileobj(image_response.raw, image_file)
-
+    drone.media.download_dir = tempfile.mkdtemp(prefix="olympe_photo_example")
+    logger.info(
+        "Download photo burst resources for media_id: {} in {}".format(
+            media_id,
+            drone.media.download_dir,
+        )
+    )
+    media_download = drone(download_media(media_id, integrity_check=True))
+    # Iterate over the downloaded media on the fly
+    resources = media_download.as_completed(expected_count=photo_count, timeout=60)
+    resource_count = 0
+    for resource in resources:
+        logger.info("Resource: {}".format(resource.resource_id))
+        if not resource.success():
+            logger.error("Failed to download {}".format(resource.resource_id))
+            continue
+        resource_count += 1
         # parse the xmp metadata
-        with open(download_path, "rb") as image_file:
+        with open(resource.download_path, "rb") as image_file:
             image_data = image_file.read()
             image_xmp_start = image_data.find(b"<x:xmpmeta")
             image_xmp_end = image_data.find(b"</x:xmpmeta")
-            image_xmp = ET.fromstring(image_data[image_xmp_start : image_xmp_end + 12])
+            if image_xmp_start < 0 or image_xmp_end < 0:
+                logger.error("Failed to find XMP photo metadata {}".format(resource.resource_id))
+                continue
+            image_xmp = ET.fromstring(image_data[image_xmp_start: image_xmp_end + 12])
             for image_meta in image_xmp[0][0]:
                 xmp_tag = re.sub(r"{[^}]*}", "", image_meta.tag)
                 xmp_value = image_meta.text
                 # only print the XMP tags we are interested in
                 if xmp_tag in XMP_TAGS_OF_INTEREST:
-                    print(resource["resource_id"], xmp_tag, xmp_value)
-
-
-
-
-def set_gimbal(drone):
-     drone(gimbal.set_target(
-    gimbal_id=0,
-    control_mode="position",
-    yaw_frame_of_reference="none",   # None instead of absolute
-    yaw=0.0,
-    pitch_frame_of_reference="absolute",
-    pitch=-90.0,
-    roll_frame_of_reference="none",     # None instead of absolute
-    roll=0.0,
-)).wait()
+                    logger.info("{} {} {}".format(resource.resource_id, xmp_tag, xmp_value))
+    logger.info("{} media resource downloaded".format(resource_count))
+    assert resource_count == 14, "resource count == {} != 14".format(resource_count)
+    assert media_download.wait(1.).success(), "Photo burst media download"
 
 
 def setup_photo_burst_mode(drone):
     drone(set_camera_mode(cam_id=0, value="photo")).wait()
     # For the file_format: jpeg is the only available option
     # dng is not supported in burst mode
-    drone(
+    assert drone(
         set_photo_mode(
             cam_id=0,
-            mode="single",
+            mode="burst",
             format="rectilinear",
             file_format="jpeg",
-            burst="burst_4_over_1s",
+            burst="burst_14_over_1s",
             bracketing="preset_1ev",
             capture_interval=0.0,
         )
-    ).wait()
+    ).wait().success()
 
 
 def main(drone):
-    takeoff(drone)
-    set_gimbal(drone)
+    drone.connect()
+    assert drone.media(
+        indexing_state(state="indexed")
+    ).wait(_timeout=60).success()
     setup_photo_burst_mode(drone)
     take_photo_burst(drone)
-    land(drone)
-    drone.disconnection()
+    drone.disconnect()
+
+
+def test_photo():
+    with olympe.Drone(DRONE_IP, media_port=DRONE_MEDIA_PORT) as drone:
+        main(drone)
+
 
 if __name__ == "__main__":
-    drone = olympe.Drone("192.168.42.1")
-    drone.connection()
-    main(drone)
+    test_photo()
